ccb172207163ae7ce49a97854609bed9f67c5f27
Dmitrii Zwei <zweidmitr@gmail.com>
2021-12-01 20:09:33 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210 #234878]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,24 +5,39 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
+import java.util.ArrayList;
+import java.util.List;
 
-        for (Element td : row) {
-            Element parent = td.parent();
-            var job = parent.child(1).text();
-            var author = parent.child(2).text();
-            var time = parent.child(5).text();
-            var shows = parent.child(4).text();
-            var link = td.child(0);
-            System.out.println("Вакансия: " + job);
-            System.out.println("Автор: " + author);
-            System.out.println("Дата: " + time);
-            System.out.println("Просмотры: " + shows);
-            System.out.println("Ссылка: " + link.attr("href"));
-            System.out.println("===============================================");
+public class SqlRuParse {
+
+    private static final String URL = "https://www.sql.ru/forum/job-offers/";
+
+    public static void main(String[] args) throws Exception {
+        int tempCount = 1;
+        List<Element> URLList = new ArrayList<>();
+        while (tempCount < 6) {
+            Document doc = Jsoup.connect(URL + tempCount).get();
+            Elements row = doc.select(".postslisttopic");
+
+            for (Element td : row) {
+                Element parent = td.parent();
+                var job = parent.child(1).text();
+                var author = parent.child(2).text();
+                var time = parent.child(5).text();
+                var shows = parent.child(4).text();
+                var link = td.child(0);
+                URLList.add(link);
+                System.out.println("Вакансия: " + job);
+                System.out.println("Автор: " + author);
+                System.out.println("Дата: " + time);
+                System.out.println("Просмотры: " + shows);
+                System.out.println("Ссылка: " + link.attr("href"));
+                System.out.println("===============================================");
+            }
+            tempCount++;
         }
+        System.out.println("===============================================");
+        System.out.println(URLList.size());
+        System.out.println(tempCount);
     }
 }
