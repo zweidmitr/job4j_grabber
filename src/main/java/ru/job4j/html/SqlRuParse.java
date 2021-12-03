6230db1bb2d014706c93f2d4da5dc24cf00ebe22
Dmitrii Zwei <zweidmitr@gmail.com>
2021-12-03 11:59:45 +0300

    2.3. Загрузка деталей поста. [#285212 #235225]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -20,40 +20,45 @@ public class SqlRuParse {
         Document doc = Jsoup.connect(url).get();
         Element header = doc.select(".messageHeader").get(0);
         Element footer = doc.select(".msgFooter").get(0);
+        Element description = doc.select(".msgBody").get(1);
+
         LocalDateTime created = new SqlRuDateTimeParser().parse(footer.text().split(" \\[")[0]);
         post.setTitle(header.text());
-        post.setName(header.text());
+        post.setDescription(description.text());
         post.setCreated(created);
         post.setLink(url);
         return post;
     }
 
     public static void main(String[] args) throws Exception {
-        int tempCount = 1;
-        List<Element> linkList = new ArrayList<>();
-        while (tempCount < 6) {
-            Document doc = Jsoup.connect(URL + tempCount).get();
-            Elements row = doc.select(".postslisttopic");
-
-            for (Element td : row) {
-                Element parent = td.parent();
-                var job = parent.child(1).text();
-                var author = parent.child(2).text();
-                var time = parent.child(5).text();
-                var shows = parent.child(4).text();
-                var link = td.child(0);
-                linkList.add(link);
-                System.out.println("Вакансия: " + job);
-                System.out.println("Автор: " + author);
-                System.out.println("Дата: " + time);
-                System.out.println("Просмотры: " + shows);
-                System.out.println("Ссылка: " + link.attr("href"));
-                System.out.println("===============================================");
-            }
-            tempCount++;
-        }
-        System.out.println("===============================================");
-        System.out.println(linkList.size());
-        System.out.println(tempCount);
-    }
+        Post post = new SqlRuParse()
+                .postLoad("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
+        System.out.println(post);
+//        int tempCount = 1;
+//        List<Element> linkList = new ArrayList<>();
+//        while (tempCount < 6) {
+//            Document doc = Jsoup.connect(URL + tempCount).get();
+//            Elements row = doc.select(".postslisttopic");
+//
+//            for (Element td : row) {
+//                Element parent = td.parent();
+//                var job = parent.child(1).text();
+//                var author = parent.child(2).text();
+//                var time = parent.child(5).text();
+//                var shows = parent.child(4).text();
+//                var link = td.child(0);
+//                linkList.add(link);
+//                System.out.println("Вакансия: " + job);
+//                System.out.println("Автор: " + author);
+//                System.out.println("Дата: " + time);
+//                System.out.println("Просмотры: " + shows);
+//                System.out.println("Ссылка: " + link.attr("href"));
+//                System.out.println("===============================================");
+//            }
+//            tempCount++;
+//        }
+//        System.out.println("===============================================");
+//        System.out.println(linkList.size());
+//        System.out.println(tempCount);
+   }
 }
