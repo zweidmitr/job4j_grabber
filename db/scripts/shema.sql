5c1a969824f40a844eb89ea54478110c315c254b
Dmitrii Zwei <zweidmitr@gmail.com>
2021-12-06 15:41:55 +0300

    4. Схема таблицы Post [#1731]

--- db/scripts/shema.sql
+++ db/scripts/shema.sql
@@ -1,7 +1,7 @@
-create table if not exist post (
+create table if not exists post (
     id serial primary key,
     name varchar(255),
     text text,
-    link varchar(255),
+    link varchar(255) unique,
     created timestamp
     );
