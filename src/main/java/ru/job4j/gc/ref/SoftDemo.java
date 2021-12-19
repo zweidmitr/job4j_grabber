286241bf6042e9aa6436810698af3a3465e5b492
Dmitrii Zwei <zweidmitr@gmail.com>
2021-12-19 11:46:27 +0300

    0. Виды ссылок [#6854]

--- src/main/java/ru/job4j/gc/ref/SoftDemo.java
+++ src/main/java/ru/job4j/gc/ref/SoftDemo.java
@@ -8,7 +8,6 @@ public class SoftDemo {
 
     public static void main(String[] args) {
         example1();
-        example2();
     }
 
     private static void example1() {
@@ -39,5 +38,27 @@ public class SoftDemo {
             }
         }
         System.out.println(liveObject);
+    }
+
+    private static void unsafe() {
+        List<SoftReference<Object>> someData = new ArrayList<>();
+        if (someData.get(0).get() != null) {
+            /* do something */
+        } else {
+            /* do something */
+        }
+        /* do something */
+        someData.get(0).get();
+    }
+
+    private static void safe() {
+        List<SoftReference<Object>> someData = new ArrayList<>();
+        Object strong = someData.get(0).get();
+        if (strong != null) {
+            /* do something */
+        } else {
+            /* do something */
+        }
+        /* work with strong */
     }
 }
