e544215133e1f59c462a6c4c3534f74875de1fca
Dmitrii Zwei <zweidmitr@gmail.com>
2021-12-20 20:08:54 +0300

    2. Что такое TDD? [#4918 #242190]

---
+++ src/main/java/tdd/Cinema3D.java
@@ -0,0 +1,22 @@
+package tdd;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Cinema3D implements Cinema {
+    @Override
+    public List<Session> find(Predicate<Session> filter) {
+        return null;
+    }
+
+    @Override
+    public Ticket buy(Account account, int row, int column, Calendar date) {
+        return new Ticket3D();
+    }
+
+    @Override
+    public void add(Session session) {
+
+    }
+}
---
+++ src/test/java/tdd/CinemaTest.java
@@ -0,0 +1,62 @@
+package tdd;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class CinemaTest {
+
+    @Ignore
+    @Test
+    public void whenBuy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, Calendar.DECEMBER, 21, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Ignore
+    @Test
+    public void whenFind() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Ignore
+    @Test
+    public void whenSessionEnding() {
+        Session session = new Session3D();
+        assertThat(session, is(new Session3D()));
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenDateIsWrong() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, Calendar.DECEMBER, 21, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenRowAndColumnIsWrong() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, Calendar.DECEMBER, 21, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+}
